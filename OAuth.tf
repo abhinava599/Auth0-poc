d24319010b4a737e8132d2b81251c7d2eb625c6d
Abhinav Anand <49289157+abhinava599@users.noreply.github.com>
2022-12-03 22:18:06 +0530

    Update OAuth.tf

--- OAuth.tf
+++ OAuth.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "auth0" {
-  domain        = "dev-1z0jxvqw0tikzzhm.us.auth0.com"
-  client_id     = "g0XdpnYPQRmhXGDucY3Sz573kwKLVSSN"
-  client_secret = "atPPjQeg2Y8pUndsh3VLYygazq_4lFvgY6RK_C-Z1a76AK0MwAricvT3U7yO8i_d"
+  domain        = "dev-1vi7cmkd3q0w5v8f.uk.auth0.com"
+  client_id     = "5JIMm1vu2ig9elmsHOPkwy8Vbnkczljd"
+  client_secret = "1N7LkUL2o068U7-f1_wXTS4jcyIR0DXZtpZVQPXZ10bfDiI-3ryFhRL2ex0t6i88"
 }
 
 
