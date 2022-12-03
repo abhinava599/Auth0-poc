e2cd3fdf2b08669117c4643160daa4b6a01c15c1
Abhinav Anand <49289157+abhinava599@users.noreply.github.com>
2022-12-03 18:44:25 +0100

    Update OAuth.tf

--- OAuth.tf
+++ OAuth.tf
@@ -8,12 +8,10 @@ terraform {
 }
 
 provider "auth0" {
-   
 }
 
-
 resource "auth0_client" "terraform-secure-express" {
-  name            = "Terraform Secure Express-demo"
+  name            = "Terraform Secure Express4"
   description     = "App for running Dockerized Express application via Terraform"
   app_type        = "regular_web"
   callbacks       = ["http://localhost:3000/callback"]
@@ -23,7 +21,3 @@ resource "auth0_client" "terraform-secure-express" {
     alg = "RS256"
   }
 }
-
-
-#domain        = "dev-1vi7cmkd3q0w5v8f.uk.auth0.com"
-  
