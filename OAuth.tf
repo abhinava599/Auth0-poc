bfa4a0c38e1c7c4dfdeca1dc7341bc126a5fb312
Abhinav Anand <49289157+abhinava599@users.noreply.github.com>
2022-12-03 22:31:54 +0530

    Update OAuth.tf

--- OAuth.tf
+++ OAuth.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "auth0" {
   domain        = "dev-1vi7cmkd3q0w5v8f.uk.auth0.com"
-  client_id     = "5JIMm1vu2ig9elmsHOPkwy8Vbnkczljd"
-  client_secret = "1N7LkUL2o068U7-f1_wXTS4jcyIR0DXZtpZVQPXZ10bfDiI-3ryFhRL2ex0t6i88"
+  client_id     = ${{ secrets.client_id }}
+  client_secret = ${{ secrets.client_secret }}
 }
 
 
